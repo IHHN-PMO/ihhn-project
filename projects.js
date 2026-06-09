d5580617f6bbf745dad45929ef3b86d58001d49b
IHHN-PMO <ihhnpmo@gmail.com>
2026-06-09 23:43:40 +0500

    Add files via upload
    IHHN Project Dashboard

---
+++ projects.js
@@ -0,0 +1,53 @@
+const dashboard = {
+
+totalProjects:70,
+
+closedLive:36,
+
+inProgress:17,
+
+notStarted:17,
+
+support:38
+
+};
+
+const projects = [
+
+{
+name:"HMIS 2.0 Encounter ER KRG",
+status:"CLOSED / LIVE",
+campus:"KRG"
+},
+
+{
+name:"HMIS 2.0 OPD Encounter KRG",
+status:"CLOSED / LIVE",
+campus:"KRG"
+},
+
+{
+name:"HMIS 2.0 ICU Module",
+status:"NOT STARTED",
+campus:"KRG"
+},
+
+{
+name:"HMIS 2.0 Procedure Management",
+status:"NOT STARTED",
+campus:"KRG"
+},
+
+{
+name:"HRMS v2.0 Recruitment",
+status:"IN PROGRESS",
+campus:"QFNST"
+},
+
+{
+name:"PACS",
+status:"CLOSED / LIVE",
+campus:"KRG"
+}
+
+];
